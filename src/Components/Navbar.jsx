9aeb04c22ca87d89a6db822b38a13784b3ff1f35
Anbumaheshwari <131516140+Anbumaheshwari@users.noreply.github.com>
2024-04-12 07:59:06 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,79 +5,50 @@ import { PiShoppingBagDuotone } from "react-icons/pi";
 import Button from '../Layout/Button';
 
 const Navbar = () => {
-  const [menu, setMenu] = useState(false);
+ 
 
-  const handleChange = () => {
-    setMenu(!menu);
-  };
-
-  const closeMenu = () => {
-    setMenu(false);
-  };
+  
  return (
        
         <header className='flex  items-center border-b border-gray-200 bg-white  p-4'>
-        <nav className='px-20 md:flex flex-row items-center text-lg font-medium gap-10'>
-        <Link
-        to="products"
-        spy={true}
-        smooth={true}
-        duration={500}
-        className="group relative inline-block cursor-pointer hover:text-primarycolor">
-        Products
-        </Link>
-        <Link
-        to="offers"
-        spy={true}
-        smooth={true}
-        duration={500}
-        className="group relative inline-block cursor-pointer hover:text-primarycolor">
-        Offers
-        </Link>
-        <Link 
-        to="offers"
-        spy={true}
-        smooth={true}
-        duration={500}
-        className="group relative inline-block cursor-pointer hover:text-primarycolor">
-        Shop
-        </Link>
-        <Link
-         to="offers"
-         spy={true}
-         smooth={true}
-         duration={500}
-         className="group relative inline-block cursor-pointer hover:text-primarycolor">
-          About
-          </Link>
+        <nav className='text-lg font-medium gap-10'>
+        <ul class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
+      <li class="mx-4 my-6 md:my-0">
+        <a href="#" class="text-xl hover:text-primarycolor duration-500">PRODUCTS</a>
+      </li>
+      <li class="mx-4 my-6 md:my-0">
+        <a href="#" class="text-xl hover:text-primarycolor duration-500">OFFER</a>
+      </li>
+      <li class="mx-4 my-6 md:my-0">
+        <a href="#" class="text-xl hover:text-primarycolor duration-500">SHOP</a>
+      </li>
+      <li class="mx-4 my-6 md:my-0">
+        <a href="#" class="text-xl hover:text-primarycolor duration-500">ABOUT</a>
+      </li>
+      </ul>
         </nav>
         <div>
-        <span className='mx-96'>
+        <span className='mx-96 text-lg font-bold'>
           DFTION
         </span>
         </div>
       
-        
+       <div className='mx-12'>  
       <span >
-          <GoSearch className='cursor-pointer' size={24}/> 
+          <GoSearch className='cursor-pointer hover:text-primarycolor' size={24}/> 
+          
           </span>
+          </div>
         
-        <div>
-          <h1 className='text-lg mx-2 cursor-pointer hover:text-primarycolor'>Search</h1>
-        </div>
+        
         <div>
         <span>
-          <PiShoppingBagDuotone className='cursor-pointer' size={24}/>
+          <PiShoppingBagDuotone className='cursor-pointer hover:text-primarycolor ' size={24}/>
           </span>
         </div>
-       <div className='mx-1'>
-          <h1 className='text-lg cursor-pointer hover:text-primarycolor'>
-            Card
-          </h1>
-        </div>
-        <div className='mx-8 hidden lg:flex'> 
-          <Button title='Login'/>
-        </div>
+       
+          
+        
         
 
       
