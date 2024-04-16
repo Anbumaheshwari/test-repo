52eece36d7dd5d79b8f05836327222ca927da273
Anbumaheshwari <131516140+Anbumaheshwari@users.noreply.github.com>
2024-04-16 09:54:18 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,61 +1,48 @@
-import React, { useState }  from 'react'
-import { Link } from 'react-scroll'
+import React, { useState } from 'react'
+import { IoMdMenu } from "react-icons/io";
 import { GoSearch } from "react-icons/go";
-import { PiShoppingBagDuotone } from "react-icons/pi";
-import Button from '../Layout/Button';
+import { RiShoppingBagLine } from "react-icons/ri";
+
 
 const Navbar = () => {
- 
+  let Links =[
+    {name:"Products",link:"/"},
+    {name:"Offer",link:"/"},
+    {name:"Shop",link:"/"},
+    {name:"About",link:"/"},
+];
+let [open,setOpen]=useState(false);
+  return (
+    <div className='shadow-md w-full fixed top-0 left-0'>
+      <div className='md:flex text-center items-center justify-between  bg-white py-3 md:px-10 px-7'>
+      <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins]'>
+      DFTION
+      </div>
+      <div onClick={()=>setOpen(!open)} 
+      className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+      <IoMdMenu name={open ? 'close':'menu'}/>
+      
+      </div>
+      <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transistion-all duration-500 ease-in ${open ? 'top-20':'top-[-490px]'} `}>
+        {
+          Links.map((Link)=>(
+            <li key={Link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+              <a href={Link.link} className='text-brightcolor hover:text-primarycolor duration-500'>{Link.name}</a>
+            </li>
+          ))
+        }
+         <span className='mx-12' >
+          <GoSearch className='cursor-pointer md:ml-12 hover:text-primarycolor' size={24}/> 
+           </span>
+           <span>
+           <RiShoppingBagLine className='cursor-pointer hover:text-primarycolor' size={24}/>
+           </span>
 
-  
- return (
-       
-        <header className='flex  items-center border-b border-gray-200 bg-white  p-4'>
-        <nav className='text-lg font-medium gap-10'>
-        <ul class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
-      <li class="mx-4 my-6 md:my-0">
-        <a href="#" class="text-xl hover:text-primarycolor duration-500">PRODUCTS</a>
-      </li>
-      <li class="mx-4 my-6 md:my-0">
-        <a href="#" class="text-xl hover:text-primarycolor duration-500">OFFER</a>
-      </li>
-      <li class="mx-4 my-6 md:my-0">
-        <a href="#" class="text-xl hover:text-primarycolor duration-500">SHOP</a>
-      </li>
-      <li class="mx-4 my-6 md:my-0">
-        <a href="#" class="text-xl hover:text-primarycolor duration-500">ABOUT</a>
-      </li>
       </ul>
-        </nav>
-        <div>
-        <span className='mx-96 text-lg font-bold'>
-          DFTION
-        </span>
-        </div>
-      
-       <div className='mx-12'>  
-      <span >
-          <GoSearch className='cursor-pointer hover:text-primarycolor' size={24}/> 
-          
-          </span>
-          </div>
-        
-        
-        <div>
-        <span>
-          <PiShoppingBagDuotone className='cursor-pointer hover:text-primarycolor ' size={24}/>
-          </span>
-        </div>
        
-          
-        
-        
-
-      
-      
-      </header>
-       
-)
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
